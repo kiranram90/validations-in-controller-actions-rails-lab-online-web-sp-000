635016b42cb96fccbe7acac657ee8e51742a05d5
kiran ram <kram@lions.molloy.edu>
2021-06-26 22:00:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
-  validates(:title, { :length => { :minimum => 2 } })
-  validates(:content, { :length => { :minimum => 100 } })
-  validates(:password, { :length => { :in => 6..20 } })
-  validates(:registration_number, { :length => { :is => 6 } })
+  validates_presence_of :title
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :content, length: { minimum: 100 }
 end
